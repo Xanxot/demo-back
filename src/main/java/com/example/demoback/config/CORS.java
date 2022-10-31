74e756240eac8ceda52283d49a7363dec73f7192
Nikita <nikit.malts@gmail.com>
2022-10-31 12:25:59 +0300

    headers

--- src/main/java/com/example/demoback/config/CORS.java
+++ src/main/java/com/example/demoback/config/CORS.java
@@ -18,7 +18,7 @@ public class CORS implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "620");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         chain.doFilter(req, res);
     }
 
